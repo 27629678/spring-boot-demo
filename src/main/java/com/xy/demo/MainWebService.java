26ae9d2371caa51b384e451f950d5420853afe6c
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2017-12-28 21:34:13 +0800

    refactor to main web service api

--- src/main/java/com/xy/demo/MainWebService.java
+++ src/main/java/com/xy/demo/MainWebService.java
@@ -2,16 +2,18 @@ package com.xy.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 
 @Controller
+@RequestMapping(value = {"/", "homepage"})
 public class MainWebService {
 
     @ResponseBody
     // 如果返回Json（或XML）等类型的数据不需要ResponseBody注解
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "greeting", method = RequestMethod.GET)
     public HashMap greeting() {
 //        return "hello, world.";
         HashMap json = new HashMap();
